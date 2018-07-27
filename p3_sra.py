553d487a676c277e56254bbdb17eef63b3555b7b
Zane Reynolds <zane.reynolds@gmail.com>
2018-07-27 13:57:14 -0400

    removed the fastq-dump option since it probably adds more confusion than it solves; also added a way to safe read the xml

--- p3_sra.py
+++ p3_sra.py
@@ -10,6 +10,27 @@ import StringIO
 from lxml import etree
 import sys
 
+def safe_read(element, xpath, index=None, xpath_fallback=None):
+
+    # the logic here is basically, try one tag, if it's not there, return an
+    # alternate tag. for either case, if there's a specific element you want
+    # (like 0), just return that instead of the list.
+
+    # also, I haven't tested to be sure this is really all that safe.  it's just
+    # safer than the way I was doing it.
+
+    val = element.xpath(xpath)
+    if val:
+        if index is not None:
+            return val[index]
+        else:
+            return val
+    elif xpath_fallback is not None:
+        return safe_read(element, xpath_fallback, index)
+    else:
+        return ''
+
+
 def get_accession_metadata(accession_id):
 
     get_cmd_template = Template('curl -s \'https://trace.ncbi.nlm.nih.gov/Traces/sra/sra.cgi?save=efetch&db=sra&rettype=docset&term=$id\'')
@@ -33,24 +54,25 @@ def get_accession_metadata(accession_id):
     exp_meta = []
     for experiment_package in tree.xpath('//EXPERIMENT_PACKAGE'):
         exp = {}
-        exp['exp_id'] = experiment_package.xpath('EXPERIMENT/@accession')[0]
-        exp['study_id'] = experiment_package.xpath('EXPERIMENT/STUDY_REF/@accession')[0]
+        exp['exp_id'] = safe_read(experiment_package, 'EXPERIMENT/@accession', index=0)
+        exp['study_id'] = safe_read(experiment_package, 'EXPERIMENT/STUDY_REF/@accession', index=0)
 
         for db in experiment_package.xpath('STUDY//XREF_LINK/DB/text()'):
             exp['study_'+db+'_ids'] = experiment_package.xpath('STUDY//XREF_LINK/ID/text()')
 
-        exp['run_ids'] = experiment_package.xpath('RUN_SET/RUN/@accession')
-        exp['library_name'] = experiment_package.xpath('EXPERIMENT//LIBRARY_NAME/text()')[0]
-        exp['library_strategy'] = experiment_package.xpath('EXPERIMENT//LIBRARY_STRATEGY/text()')[0]
+        exp['run_ids'] = safe_read(experiment_package, 'RUN_SET/RUN/@accession')
+        exp['library_name'] = safe_read(experiment_package, 'EXPERIMENT//LIBRARY_NAME/text()', index=0)
+        exp['library_strategy'] = safe_read(experiment_package, 'EXPERIMENT//LIBRARY_STRATEGY/text()', index=0)
 
         # just assume one sample
-        sample = experiment_package.xpath('SAMPLE')[0]
+        sample = safe_read(experiment_package, 'SAMPLE', index=0)
         exp['sample_id'] = sample.attrib['accession']
-        exp['sample_description'] = sample.xpath('DESCRIPTION/text()')[0]
-        exp['sample_organism'] = sample.xpath('SAMPLE_NAME/SCIENTIFIC_NAME/text()')[0]
-        exp['sample_taxon'] = sample.xpath('SAMPLE_NAME/TAXON_ID/text()')[0]
-        sample_attrib_tags = sample.xpath('SAMPLE_ATTRIBUTES/SAMPLE_ATTRIBUTE/TAG/text()')
-        sample_attrib_vals = sample.xpath('SAMPLE_ATTRIBUTES/SAMPLE_ATTRIBUTE/VALUE/text()')
+        exp['sample_title'] = safe_read(sample, 'TITLE/text()', index=0)
+        exp['sample_description'] = safe_read(sample, 'DESCRIPTION/text()', index=0)
+        exp['sample_organism'] = safe_read(sample, 'SAMPLE_NAME/SCIENTIFIC_NAME/text()', index=0)
+        exp['sample_taxon'] = safe_read(sample, 'SAMPLE_NAME/TAXON_ID/text()', index=0)
+        sample_attrib_tags = safe_read(sample, 'SAMPLE_ATTRIBUTES/SAMPLE_ATTRIBUTE/TAG/text()')
+        sample_attrib_vals = safe_read(sample, 'SAMPLE_ATTRIBUTES/SAMPLE_ATTRIBUTE/VALUE/text()')
         sample_attribs = zip(sample_attrib_tags, sample_attrib_vals)
         for sample_attrib in sample_attribs:
             # prepend sample_ for each key related to the sample (unless it's already there)
@@ -82,23 +104,19 @@ def get_run_ids(run_meta):
         run_ids.append(run['run_id'])
     return run_ids
 
-def download_fastq_files(fastq_dump_loc, output_dir, run_ids, gzip_output=True, use_fastq_dump=False):
+def download_fastq_files(fasterq_dump_loc, output_dir, run_ids, gzip_output=True):
 
-    if use_fastq_dump:
-        # use the old fastq-dump utility
-        fastq_dump_cmd_template = Template('$fastq_dump_bin/fastq-dump -I --skip-technical --split-files --read-filter pass -outdir $out_dir $id')
-    else:
-        # use the new fasterq-dump utility (faster, but doesn't have filtering option which results in bigger files)
-        fastq_dump_cmd_template = Template('$fastq_dump_bin/fasterq-dump --outdir $out_dir --split-files -f $id')
+    # use the new fasterq-dump utility (faster, but doesn't have filtering option which results in bigger files)
+    fasterq_dump_cmd_template = Template('$fasterq_dump_bin --outdir $out_dir --split-files -f $id | gzip')
 
     # for each run, download the fastq file
     for run_id in run_ids:
-        fastq_dump_cmd = fastq_dump_cmd_template.substitute(fastq_dump_bin=fastq_dump_loc, id=run_id, out_dir=output_dir)
+        fasterq_dump_cmd = fasterq_dump_cmd_template.substitute(fasterq_dump_bin=fasterq_dump_loc, id=run_id, out_dir=output_dir)
 
-        # call fastq-dump
+        # call fasterq-dump
         try:
-            print 'executing \'' + fastq_dump_cmd + '\''
-            result = subprocess.check_output([fastq_dump_cmd], shell=True)
+            print 'executing \'' + fasterq_dump_cmd + '\''
+            result = subprocess.check_output([fasterq_dump_cmd], shell=True)
         except subprocess.CalledProcessError as e:
             return_code = e.returncode
 
@@ -109,7 +127,7 @@ def download_fastq_files(fastq_dump_loc, output_dir, run_ids, gzip_output=True, 
 
     return
 
-def download_sra_data(fastq_dump_loc, fastq_output_dir, accession_id, metaonly, compress_files, filter_reads):
+def download_sra_data(fasterq_dump_loc, fastq_output_dir, accession_id, metaonly, compress_files):
 
     # ===== 1. Get the metadata for each run
     metadata = get_accession_metadata(accession_id)
@@ -118,7 +136,7 @@ def download_sra_data(fastq_dump_loc, fastq_output_dir, accession_id, metaonly, 
 
     # ===== 2. Get the fastq files for each run
     if not metaonly:
-        download_fastq_files(fastq_dump_loc, fastq_output_dir, get_run_ids(metadata), gzip_output=compress_files, use_fastq_dump=filter_reads)
+        download_fastq_files(fasterq_dump_loc, fastq_output_dir, get_run_ids(metadata), gzip_output=compress_files)
         print 'Fastq Filenames:'
         print(glob.glob(fastq_output_dir+'/*.fastq*'))
 
@@ -131,20 +149,19 @@ def download_sra_data(fastq_dump_loc, fastq_output_dir, accession_id, metaonly, 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='A script to gather SRA data for a given accession id.',
-                usage='usage: ./p3_sra.py -bin <path to fastq-dump> -out <fastq output directory> -id <SRA accession id (SRX, SRP, SRR)>')
+                usage='usage: ./p3_sra.py -bin <path to fasterq-dump> -out <fastq output directory> -id <SRA accession id (SRX, SRP, SRR)>')
 
-    parser.add_argument('-tooldir', required=True, help='Path to the fastq-dump binary')
+    parser.add_argument('-bin', required=True, help='Path to the fasterq-dump binary')
     parser.add_argument('-out', required=True, help='Temporary output directory for fastq files')
     parser.add_argument('-id', required=True, help='SRA accession id (SRX, SRP, SRR)')
     parser.add_argument('--metaonly', action='store_true', help='Skip the download of the fastq files')
     parser.add_argument('--gzip', action='store_true', help='Compress the fastq files after download')
-    parser.add_argument('--filter', action='store_true', help='Use fastq-dump instead of fasterq-dump to leverage the read filter')
 
     args = parser.parse_args()
 
     accession_id = args.id
     acceptable_prefixes = ('SRX', 'SRP', 'SRR', 'DRX', 'DRP', 'DRR')
     if accession_id.startswith(acceptable_prefixes):
-        download_sra_data(args.tooldir, args.out, accession_id, args.metaonly, args.gzip, args.filter)
+        download_sra_data(args.bin, args.out, accession_id, args.metaonly, args.gzip)
     else:
         sys.exit('Accession ID must start with: ' + ', '.join(acceptable_prefixes))
